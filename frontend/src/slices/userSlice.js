3b7acb94156ad5a075857441a33e5f0239ea7b0a
Hudson Santos <hudsnss@gmail.com>
2022-12-05 06:06:43 -0300

    update user slice

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -21,6 +21,22 @@ export const profile = createAsyncThunk(
     }
 )
 
+// Update user details
+export const UpdateProfile = createAsyncThunk(
+    "user/update",
+    async (user, thunkAPI) => {
+        const token = thunkAPI.getState.auth.user.token
+
+        const data = await userService.updateProfile(user, token)
+
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
 export const userSlice = createSlice({
     name: "user",
     initialState,
@@ -38,6 +54,19 @@ export const userSlice = createSlice({
             state.success = true
             state.error = null
             state.user = action.payload
+        }).addCase(updateProfile.pending, state => {
+            state.loading = true
+            state.error = false
+        }).addCase(updateProfile.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.user = action.payload
+            state.message = "Usuário atualizado com sucesso!"
+        }).addCase(updateProfile.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload
+            state.user = null
         })
     }
 })
